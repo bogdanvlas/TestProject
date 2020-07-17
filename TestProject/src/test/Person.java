91c49286addc3c886fa7af0897b7fddb2009809b
Bogdan <vlasenko_b@itstep.academy>
2020-07-17 20:05:52 +0300

    add Person

---
+++ TestProject/src/test/Person.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Person {
+	String name;
+	public Person(String name) {
+		this.name=name;
+	}
+	@Override
+	public String toString() {
+		return "Person [name=" + name + "]";
+	}
+	
+}
